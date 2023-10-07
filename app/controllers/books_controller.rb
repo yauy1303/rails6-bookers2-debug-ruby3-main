55d92c5227804443608c3296f1df92044a974b9f
yayu1303 <62428626+yauy1303@users.noreply.github.com>
2023-10-07 12:03:18 +0000

    10/7

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,13 @@ class BooksController < ApplicationController
 
   def index
     @book = Book.new
-    @books = Book.all
+    # @books = Book.all
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorited_users).
+      sort_by {|x|
+        x.favorited_users.includes(:favorites).where(created_at: from...to).size
+      }.reverse
   end
 
   def create
